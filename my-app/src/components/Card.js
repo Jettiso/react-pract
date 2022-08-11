ebc9deb7abe359d3ffda7f9d86bfbcc1c7f999b3
Jettison <ilmsmjettison@gmail.com>
2022-08-11 11:35:31 +0800

    final save

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
 export default function Card(props) {
-    console.log(props)
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = 'SOLD OUT'
+    }
+    else if (props.location === 'Online') {
+        badgeText = 'ONLINE'
+    }
     return (
         <div className='card'>
-            <img src={`../images/${props.img}`} className='card--image' alt='katie'/>
+            {badgeText && <div className='card--badge'>{badgeText} </div>}
+            <img src={`../images/${props.coverImg}`} className='card--image' alt='katie'/>
             <div className='card--stats'>
                 <img src='../images/star.png' alt='star' className='card--star'/>
-                <span>{props.rating}</span>
-                <span className='gray'>({props.reviewCount}) • </span>
-                <span className='gray'>{props.country}</span>
+                <span>{props.stats.rating}</span>
+                <span className='gray'>({props.stats.reviewCount}) • </span>
+                <span className='gray'>{props.location}</span>
             </div>
             <p>{props.title}</p> 
             <p><span className='bold'>From ${props.price}</span> / person</p> 
